5967e57d982c7f36fac667dbac9cfd593062cadc
Izalala <86750551+IzalalaCoder@users.noreply.github.com>
2024-02-14 15:32:49 +0100

    Create HandCircle.java

---
+++ TP2/response/shapes/HandCircle.java
@@ -0,0 +1,16 @@
+package shapes;
+
+public class HandCircle implements Circle {
+
+  public HandCircle() {
+  }
+  
+  public double getRadius() {
+    return 0.0;
+  }
+
+  public void draw(Graphics2D graphic) {
+  
+  }
+
+}
