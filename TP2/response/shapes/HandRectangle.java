310dc608caad22de3ce1151805b6d136b3f9edc4
Izalala <86750551+IzalalaCoder@users.noreply.github.com>
2024-02-14 15:35:44 +0100

    Create HandRectangle.java

---
+++ TP2/response/shapes/HandRectangle.java
@@ -0,0 +1,20 @@
+package shapes;
+
+public class HandRectangle implements Rectangle {
+
+  public HandRectangle() {
+  }
+  
+  public double getWidth() {
+    return 0.0;
+  }
+  
+  public double getHeight() {
+    return 0.0;
+  }
+
+  public void draw(Graphics2D graphic) {
+    
+  }
+
+}
