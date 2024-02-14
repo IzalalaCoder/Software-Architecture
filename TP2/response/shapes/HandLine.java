7bd26ca16892e6df23a519fb14546888282b5a08
Izalala <86750551+IzalalaCoder@users.noreply.github.com>
2024-02-14 15:30:33 +0100

    Create HandLine.java

---
+++ TP2/response/shapes/HandLine.java
@@ -0,0 +1,16 @@
+package shapes;
+
+public class HandLine implements Line {
+
+  public HandLine() {
+  }
+  
+  public double getLength() {
+    return 0.0;
+  }
+
+  public void draw(Graphics2D graphic) {
+  
+  }
+
+}
