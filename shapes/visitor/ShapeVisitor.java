62a643c85269c2c390218160b19f90797948d2c8
Izalala <86750551+IzalalaCoder@users.noreply.github.com>
2024-03-13 15:21:10 +0100

    Update ShapeVisitor.java

--- shapes/visitor/ShapeVisitor.java
+++ shapes/visitor/ShapeVisitor.java
@@ -1,1 +1,11 @@
+package shapes.visitor;
 
+public interface ShapeVisitor {
+  public void VisitStrictLine(ShapeVisitor shapeVisitor);
+  public void VisitStrictRectangle(ShapeVisitor shapeVisitor);
+  public void VisitStrictCircle(ShapeVisitor shapeVisitor);
+  public void VisitGroupShape(ShapeVisitor shapeVisitor);
+  public void VisitHandLine(ShapeVisitor shapeVisitor);
+  public void VisitHandRectangle(ShapeVisitor shapeVisitor);
+  public void VisitHandCircle(ShapeVisitor shapeVisitor);
+}
