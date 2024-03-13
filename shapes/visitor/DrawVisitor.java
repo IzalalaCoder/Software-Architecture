4727f9f467b5ecdc480110062dc49d031edfa243
Izalala <86750551+IzalalaCoder@users.noreply.github.com>
2024-03-13 15:35:14 +0100

    Update DrawVisitor.java

--- shapes/visitor/DrawVisitor.java
+++ shapes/visitor/DrawVisitor.java
@@ -2,6 +2,12 @@ package shapes.visitor;
 
 public class DrawVisitor implements ShapeVisitor {
 
-    
+    public void VisitStrictLine(StrictLine strictLine);
+  public void VisitStrictRectangle(StrictRectangle strictRectangle);
+  public void VisitStrictCircle(StrictCircle strictCircle);
+  public void VisitGroups(Groups groups);
+  public void VisitHandLine(HandLine handLine);
+  public void VisitHandRectangle(HandRectangle handRectangle);
+  public void VisitHandCircle(HandCircle handCircle);
   
 }
